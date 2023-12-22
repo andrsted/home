de857c42f4a3923ab8529b97cf6f8c1155f27024
andrsted <andrsted@gmail.com>
2023-12-22 18:40:14 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Chat, ShoppingCart, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Chat16Regular, ShoppingCart, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -65,7 +65,7 @@ const siteLinksList = computed(() => {
 
 // 网站链接图标
 const siteIcon = {
-  Chat,
+  Chat16Regular,
   ShoppingCart,
   CompactDisc,
   Compass,
